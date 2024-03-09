a881f1a70f955f3ec2a20b17950cd47106753b49
Brian-Ambani <brainambani1@gmail.com>
2024-03-10 02:07:42 +0300

    add a middle and a fundtion to handle errors

--- api/index.js
+++ api/index.js
@@ -23,3 +23,13 @@ mongoose
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    sucess: false,
+    message,
+    statusCode,
+  });
+});
